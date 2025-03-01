7677ebc4560663465b6cfaa14594e8951b92ea0c
heistifeh <teefeh26@gmail.com>
2025-03-01 16:18:31 +0100

    update header section with image and Private the profile page

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,6 +3,7 @@ import userReducer from "./user/userSlice";
 import storage from "redux-persist/lib/storage";
 import { persistReducer } from "redux-persist";
 import persistStore from "redux-persist/es/persistStore";
+
 const rootReducer = combineReducers({ user: userReducer });
 const persistConfig = {
   key: "root",
