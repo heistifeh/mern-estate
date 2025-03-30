24684cf92c4e72ecfa01f9b51b318674bb98761e
heistifeh <teefeh26@gmail.com>
2025-03-30 18:35:59 +0100

    update a listing route

--- api/controller/listing.controller.js
+++ api/controller/listing.controller.js
@@ -18,12 +18,29 @@ export const deleteListing = async (req, res, next) => {
     const listing = await Listing.findById(req.params.id);
     console.log(req.user.id, listing.userRef);
     if (!listing) return next(errorHandler(404, "Listing not found"));
-    
+
     if (req.user.id !== listing.userRef)
       return next(errorHandler(403, "You can only delete your own listing😒"));
 
     await Listing.findByIdAndDelete(req.params.id);
     return res.status(200).json("Listing has been deleted🎉");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) return next(errorHandler(404, "Listing not found"));
+    if (req.user.id !== listing.userRef)
+      return next(errorHandler(403, "You can only update your own listing😒"));
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    return res.status(200).json(updatedListing);
   } catch (error) {
     next(error);
   }
