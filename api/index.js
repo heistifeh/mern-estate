94657b1223d41680c201ddeeabd554e5e21dcd9a
heistifeh <teefeh26@gmail.com>
2025-05-12 08:48:15 +0100

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import listingRouter from "./routes/listing.routes.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -15,6 +16,7 @@ mongoose
     console.log(err);
   });
 
+  const __dirname = path.resolve();
 const app = express();
 app.use(express.json()); //allow json from insomnia
 app.use(cookieParser());
@@ -26,6 +28,10 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/dist/index.html"));
+});
 // middlware for error
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
